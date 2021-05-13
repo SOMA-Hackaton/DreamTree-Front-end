1d9a5cf957675fcde49c495160eb061148fb1ac3
anteater-wsl <anteater1056@gmail.com>
2021-05-13 22:39:48 +0900

    [feat] api 서버 통신 후 마커 표시 구현

--- src/service/api.js
+++ src/service/api.js
@@ -1,3 +1,17 @@
 import axiosResource from "./axios";
+
 export class Api {
+    static async getAllStores () {
+        try {
+            const response = await axiosResource.get()
+            return response.data
+        }
+        catch (err) {
+            /**
+             * NetworkError
+             */
+            console.log(err)
+            return 0;
+        }
+    }
 }
--- src/service/axios.js
+++ src/service/axios.js
@@ -3,9 +3,10 @@ import axios from "axios";
 const axiosResource = axios.create({
     baseURL:
     process.env.VUE_APP_NODE_ENV != "production"
-      ? "http://localhost:8080/api"
+      ? "https://dreamtree-dywzy.run.goorm.io/"
       : process.env.VUE_APP_AUTH_URL,
   withCredentials: true,
+  headers: { 'Access-Control-Allow-Origin': '*' }
 });
 
 export default axiosResource;
